9007296f021fb20612760159b4844cf2991eb325
EmperorJordan <jordan.ton9@gmail.com>
2016-10-01 19:37:30 -0700

    Change table name from jobs to job_postings

--- src/main/java/usjobs/model/JobPosting.java
+++ src/main/java/usjobs/model/JobPosting.java
@@ -20,7 +20,7 @@ import javax.persistence.Table;
 import org.hibernate.annotations.Type;
 
 @Entity
-@Table(name = "jobs")
+@Table(name = "job_postings")
 public class JobPosting implements Serializable {
 
 	private static final long serialVersionUID = 1L;
