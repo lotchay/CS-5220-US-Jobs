849a675705f19089218c34e50fa97243b14ff043
EmperorJordan <jordan.ton9@gmail.com>
2016-10-13 15:29:55 -0700

    Create three scripts to create tables, drop tables, and usjobs.ddl

---
+++ src/main/scripts/usjobs-drop.sql
@@ -0,0 +1,12 @@
+drop sequence if exists hibernate_sequence;
+drop table if exists applications;
+drop table if exists degrees;
+drop table if exists employer_reviews;
+drop table if exists favorites;
+drop table if exists job_postings cascade;
+drop table if exists job_reviews;
+drop table if exists jobs_applied;
+drop table if exists resumes;
+drop table if exists user_phones;
+drop table if exists user_roles;
+drop table if exists users;
