5cba5c20ca2cdc9e98f63c9f72b5448f289d9e1e
jsunthon <jsunthon@gmail.com>
2016-10-20 13:25:49 -0700

    add date posted to a job when created;made the job posting page look better

--- src/main/java/usjobs/web/controller/ProfileController.java
+++ src/main/java/usjobs/web/controller/ProfileController.java
@@ -1,5 +1,7 @@
 package usjobs.web.controller;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -64,6 +66,8 @@ public class ProfileController {
     
 	@RequestMapping(value = "/user/addJob.html", method = RequestMethod.POST)
 	public String addJob(@RequestParam int employerId, @ModelAttribute("newJob") JobPosting newJob, SessionStatus session) {
+		Date date = new Date();
+		newJob.setDatePosted(date);
 		jobPostingDao.save(newJob);
 		session.setComplete();
 		return "redirect:profile.html?id=" + employerId;
