379102248a8e5a129c2d5962b42d4a8cd74fea21
EmperorJordan <jordan.ton9@gmail.com>
2016-10-20 22:14:52 -0700

    Fix small error in user validator and add password confirmation to add.jsp

--- src/main/java/usjobs/web/validator/UserValidator.java
+++ src/main/java/usjobs/web/validator/UserValidator.java
@@ -27,38 +27,45 @@ public class UserValidator implements Validator {
         User user = (User) target;
         
         if ( !StringUtils.hasText( user.getUsername() ) ) {
+            
             errors.rejectValue( "username", "error.field.empty" );
         }
 
         if ( !StringUtils.hasText( user.getPassword() ) ) {
+            
             errors.rejectValue( "password", "error.field.empty" );
         }
         
-
         if ( !StringUtils.hasText( user.getPassword2() ) ) {
-            errors.rejectValue( "password", "error.field.empty" );
+            
+            errors.rejectValue( "password2", "error.field.empty" );
         }
         
         if ( !StringUtils.hasText( user.getFirstName() ) ) {
+            
             errors.rejectValue( "firstName", "error.field.empty" );
         }
         
         if ( !StringUtils.hasText( user.getLastName() ) ) {
+            
             errors.rejectValue( "lastName", "error.field.empty" );
         }
         
         if ( !StringUtils.hasText( user.getEmail() ) ) {
+            
             errors.rejectValue( "email", "error.field.empty" );
         }
         
         if ( ( user.getPassword() != null ) && 
             !( user.getPassword().equals(user.getPassword2() ) ) ) {
+            
             errors.rejectValue( "password2", "error.password.doesnotmatch" );
         }
         
         String username = user.getUsername();
         
         if ( ( username != null ) && ( userDao.getUser( username ) != null ) ) {
+            
             errors.rejectValue( "username", "error.username.taken" );
         }
     }
