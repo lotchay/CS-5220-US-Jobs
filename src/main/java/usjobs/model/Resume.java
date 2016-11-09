dd42696dd562795a1cb2b5a83f51f05ec9596580
EmperorJordan <jordan.ton9@gmail.com>
2016-11-08 22:21:00 -0800

    Add tsvector columns, triggers, and create index on the tsvector columns for job_postings and resumes tables; Add content for resume model.

--- src/main/java/usjobs/model/Resume.java
+++ src/main/java/usjobs/model/Resume.java
@@ -11,91 +11,118 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.Type;
+
 @Entity
-@Table(name="resumes")
+@Table(name = "resumes")
 public class Resume implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@Column(name = "resume_id")
-	@GeneratedValue
-	private Integer id;
+    @Id
+    @Column(name = "resume_id")
+    @GeneratedValue
+    private Integer id;
 
-	private String name;
+    private String name;
 
-	@Column(name = "upload_date")
-	private Date uploadDate;
-	
-	@Column(name = "file_path")
-	private String filePath;
+    @Column(name = "upload_date")
+    private Date uploadDate;
 
-	@Column(name = "file_name")
-	private String fileName;
+    @Column(name = "file_path")
+    private String filePath;
 
-	@Column(name = "file_data")
-	private byte[] fileData;
+    @Column(name = "file_name")
+    private String fileName;
 
-	@ManyToOne
-	@JoinColumn(name = "user_id")
-	private User user;
+    @Column(name = "file_data")
+    private byte[] fileData;
 
-	public Integer getId() {
-		return id;
-	}
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    @Type(type = "text")
+    private String content;
 
-	public String getName() {
-		return name;
-	}
+    public Integer getId() {
 
-	public void setName(String name) {
-		this.name = name;
-	}
+        return id;
+    }
 
-	public Date getUploadDate() {
-		return uploadDate;
-	}
+    public void setId( Integer id ) {
 
-	public void setUploadDate(Date uploadDate) {
-		this.uploadDate = uploadDate;
-	}
+        this.id = id;
+    }
 
-	public String getFileName() {
-		return fileName;
-	}
+    public String getName() {
 
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
+        return name;
+    }
 
-	public byte[] getFileData() {
-		return fileData;
-	}
+    public void setName( String name ) {
 
-	public void setFileData(byte[] fileData) {
-		this.fileData = fileData;
-	}
+        this.name = name;
+    }
 
-	public User getUser() {
-		return user;
-	}
+    public Date getUploadDate() {
 
-	public void setUser(User user) {
-		this.user = user;
-	}
+        return uploadDate;
+    }
 
-	public String getFilePath() {
-		return filePath;
-	}
+    public void setUploadDate( Date uploadDate ) {
 
-	public void setFilePath(String filePath) {
-		this.filePath = filePath;
-	}
-	
-	
-	
+        this.uploadDate = uploadDate;
+    }
+
+    public String getFileName() {
+
+        return fileName;
+    }
+
+    public void setFileName( String fileName ) {
+
+        this.fileName = fileName;
+    }
+
+    public byte[] getFileData() {
+
+        return fileData;
+    }
+
+    public void setFileData( byte[] fileData ) {
+
+        this.fileData = fileData;
+    }
+
+    public User getUser() {
+
+        return user;
+    }
+
+    public void setUser( User user ) {
+
+        this.user = user;
+    }
+
+    public String getFilePath() {
+
+        return filePath;
+    }
+
+    public void setFilePath( String filePath ) {
+
+        this.filePath = filePath;
+    }
+
+    public String getContent() {
+
+        return content;
+    }
+
+    public void setContent( String content ) {
+
+        this.content = content;
+    }
+
 }
