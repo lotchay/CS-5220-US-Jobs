76f8df34c7d31e52387dec9e2ae3b9e53b3e32d5
James <jsunthon@gmail.com>
2016-11-29 20:42:26 -0800

    styling/js works, and jquery ui included in bower. added a service

---
+++ src/main/java/usjobs/web/service/JobPostingService.java
@@ -0,0 +1,30 @@
+package usjobs.web.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import usjobs.model.JobPosting;
+import usjobs.model.dao.JobPostingDao;
+
+@RestController
+public class JobPostingService {
+	
+	@Autowired
+	JobPostingDao jobPostingDao;
+	
+	@RequestMapping(value="/service/job/{id}", method=RequestMethod.GET)
+	public JobPosting getJobPosting(@PathVariable int id) {
+		return jobPostingDao.getJobPosting(id);
+	}
+	
+	@RequestMapping(value="/service/jobs", method=RequestMethod.GET)
+	public List<JobPosting> getJobPostings() {
+		return jobPostingDao.getJobPostings();
+	}
+}
